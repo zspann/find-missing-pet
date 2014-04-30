85a954a9189f42dea6911f3d10ec05f2b159ab57
ahimmelstoss <ahimmelstoss@gmail.com>
2014-04-30 11:52:39 -0400

    animals in their right places

--- test.rb
+++ test.rb
@@ -7,13 +7,17 @@ class Animal
     :alley_cat => File.exists?('./cats/indoor/back-alley/alley-cat.gif'),
     :lion => File.exists?('./cats/outdoor/africa/lion.jpg'),
     :african_wild_cat => File.exists?('./cats/outdoor/africa/african-wild-cat.jpg'),
-    :north_american_cat => File.exists?('./cats/outdoor/north-america/north_american_wild_cat.jpg'),
+    :north_american_wild_cat => File.exists?('./cats/outdoor/north-america/north-american-wild-cat.jpg'),
     :grandma_cat => File.exists?('./cats/indoor/grandmas-house/grandma-cat.jpg'),
     :herding_dog => File.exists?('./dogs/domestic/herding/herding_dog.jpg'),
     :hound_dog => File.exists?('./dogs/domestic/hound/hound_dog.jpg'),
     :toy_dog => File.exists?('./dogs/domestic/toy/toy_dog.jpg'),
-    :australian_wild_dog => File.exists?('./dogs/wild/australia/australian_wild_dog.jpg'),
-
+    :australian_wild_dog => File.exists?('./dogs/wild/australia/australian-wild-dog.jpg'),
+    :wolf => File.exists?('./dogs/wild/USA/wolf.jpg'),
+    :circus_elephant => File.exists?('./elephants/circus/circus-elephant.jpg'),
+    :painting_elephant => File.exists?('./elephants/painters/painting-elephant.jpg'),
+    :caged_iguana => File.exists?('./iguanas/caged/caged-iguana.jpg'),
+    :free_roam_iguana => File.exists?('./iguanas/free-roam/free-roaming iguana.jpg')
   }
 
   def self.create_name(animal)
